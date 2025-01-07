33581ad2beb27849b42dc351a22e69b24fb92e7a
Ryan James <ryanjames1729@gmail.com>
2025-01-07 12:33:29 -0500

    updated starter code

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
-public class Main {
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+      
+          int[] arr = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6};
+  
+          String[] message = {"1", "Problem", "2", "Solving", "3", "Requires", "4", "Iteration"};
+  
+  
     }
-}
+  }
